be6989383b04ecef29104ab7364b71c498cb2b37
JR-Koders <jules.caoeiros@gmail.com>
2024-06-14 10:57:11 -0400

    Fix Footer appearance
    Footer touches now the bottom of the page, and only its top corners are rounded

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 mx-auto sm:px-0 md:w-2/3 lg:w-1/2">
+        <footer className="bg-white rounded-t-lg shadow dark:bg-gray-900 mx-auto sm:px-0 md:w-2/3 lg:w-1/2">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
